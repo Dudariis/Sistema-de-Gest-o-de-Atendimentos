ece5b070743257efc12bf4a0a4106bb29a5044fa
MiniEves <evelynks2004@gmail.com>
2026-01-29 09:26:55 -0300

    Início de ptojeto

---
+++ Modelos/Usuario.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace helpdesk;
+
+public abstract class Usuario
+{
+    public int Id {get; protected set;}
+    public string Nome {get; protected set;}
+    public string Email {get; protected set;}
+
+    protected Usuario (int id, string nome, string email)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+    }
+    public abstract string ExibirDados();
+}
